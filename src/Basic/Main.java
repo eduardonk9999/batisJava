cbf36a4280d8340ebc1bacf1891711803a693a28
eduardonk9999 <thiago.novak7@gmail.com>
2025-11-17 08:44:39 -0300

    tipos de dados

--- src/Basic/Main.java
+++ src/Basic/Main.java
@@ -1,10 +1,7 @@
+package Basic;
+
 public class Main {
     public static void main(String[] args) {
-
-
-
-
-
 
     }
 }
---
+++ src/Basic/TipoDeDados/DadosPrimitivos.java
@@ -0,0 +1,18 @@
+package Basic.TipoDeDados;
+
+public class DadosPrimitivos {
+    public static void main(String[] args) {
+        // Dados Primitivos
+        /*
+         * int: Números inteiros
+         * float: Números de ponto flutuante
+         * double: Números de ponto flutuante de precisão dupla.
+         * char: Um único caractere.
+         * boolean: Valores verdadeiros ou falsos.
+         * byte: Números inteiros pequenos
+         * short: Números inteiros menores que um int
+         * Long: Números inteiros maiores que um int
+         * */
+
+    }
+}
